a967305e8793fcf022ec90a7c3274ccebe2d3fb9
longnghia <41385034+longnghia@users.noreply.github.com>
2024-11-13 11:29:15 +0700

    Add snackbar duration

--- app/src/main/java/com/paulcoding/pindownloader/ui/page/home/HomePage.kt
+++ app/src/main/java/com/paulcoding/pindownloader/ui/page/home/HomePage.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
@@ -89,7 +90,8 @@ fun HomePage(
 
             val result = snackbarHostState.showSnackbar(
                 message,
-                actionLabel
+                actionLabel,
+                duration = SnackbarDuration.Short
             )
             when (result) {
                 SnackbarResult.ActionPerformed -> {

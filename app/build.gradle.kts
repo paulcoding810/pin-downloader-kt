036b0af3c7a470fb8712ebb3a3237561be6be29a
Paul <41385034+paulcoding810@users.noreply.github.com>
2025-09-27 20:42:12 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.paulcoding.pindownloader"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.paulcoding.pindownloader"
